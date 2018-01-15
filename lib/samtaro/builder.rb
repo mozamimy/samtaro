4c40d5a6a2af997391db5e0391cc28563753244d
mozamimy (Moza USANE) <alice@mozami.me>
2018-01-15 12:58:49 +0900

    Improve build subcommand

--- lib/samtaro/builder.rb
+++ lib/samtaro/builder.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
-require 'tmpdir'
 require 'open3'
+require 'fileutils'
 
 module Samtaro
   class Builder
     # @param [String] runtime
-    def initialize(runtime:, directory:, logger:)
+    def initialize(source_dir:, artifact_dir:, runtime:, logger:)
+      @source_dir = source_dir
+      @artifact_dir = artifact_dir
       @runtime = runtime
-      @directory = directory
       @logger= logger
 
       validate_params!
@@ -17,7 +18,11 @@ module Samtaro
     def build
       case @runtime
       when 'nodejs6.10'
-        Nodejs.new(@directory, @logger).build
+        Nodejs.new(
+          source_dir: @source_dir,
+          artifact_dir: @artifact_dir,
+          logger: @logger,
+        ).build
       else
         raise "Unknown runtime: #{@runtime}"
       end
@@ -26,42 +31,46 @@ module Samtaro
     private
 
     def validate_params!
-      if @runtime.nil?
-        raise 'runtime should not be nil.'
-      end
-
-      if @directory.nil?
-        raise 'directory should not be nil.'
-      end
+      raise 'artifact_dir should not be nil.' if @artifact_dir.nil?
+      raise 'source_dir should not be nil.' if @source_dir.nil?
+      raise 'runtime should not be nil.' if @runtime.nil?
     end
 
     class Nodejs
-      def initialize(directory, logger)
-        @directory = directory
+      def initialize(source_dir:, artifact_dir:, logger:)
+        @source_dir = source_dir
+        @artifact_dir = artifact_dir
         @logger = logger
       end
 
       def build
         @logger.info('Begin to build with Node.js 6.10 environment...')
 
-        Dir.mktmpdir do |tmpdir|
-          FileUtils.cp_r(Dir.glob("#{@directory}/*"), tmpdir)
-          FileUtils.cp_r(Dir.glob("#{File.dirname(__FILE__)}/../../docker/nodejs6.10/*"), tmpdir)
+        if Dir.exist?(@artifact_dir)
+          FileUtils.rm_r(@artifact_dir)
+          @logger.debug("#{@artifact_dir} is deleted.")
+        end
 
-          Dir.chdir(tmpdir)
+        FileUtils.mkdir(@artifact_dir)
+        @logger.debug("#{@artifact_dir} is created.")
 
-          Open3.popen3('docker-compose build && docker-compose run amazonlinux') do |i, o, e, w|
-            o.each do |line|
-              @logger.info(line.chomp)
-            end
+        FileUtils.cp_r(Dir.glob("#{@source_dir}/*"), @artifact_dir)
+        FileUtils.cp_r(Dir.glob("#{File.dirname(__FILE__)}/../../docker/nodejs6.10/*"), @artifact_dir)
+        @logger.debug('Files for build are copied.')
 
-            e.each do |line|
-              @logger.info(line.chomp)
-            end
+        Dir.chdir(@artifact_dir)
+
+        Open3.popen3('docker-compose build && docker-compose run amazonlinux') do |i, o, e, w|
+          o.each do |line|
+            @logger.info(line.chomp)
           end
 
-          @logger.info(Dir.entries(tmpdir))
+          e.each do |line|
+            @logger.info(line.chomp)
+          end
         end
+
+        @logger.debug(Dir.entries('.'))
       end
     end
   end
--- lib/samtaro/cli.rb
+++ lib/samtaro/cli.rb
@@ -58,9 +58,10 @@ module Samtaro
           Samtaro.logger.level = Logger::DEBUG
         end
 
-        builder= Builder.new(
-          runtime: @runtime,
-          directory: @directory,
+        builder = Builder.new(
+          source_dir: @source_dir || './src',
+          artifact_dir: @artifact_dir || './build',
+          runtime: @runtime || 'nodejs6.10',
           logger: Samtaro.logger,
         )
         builder.build
@@ -75,8 +76,9 @@ module Samtaro
         @parser ||= OptionParser.new do |opts|
           opts.banner = 'samtaro build [OPTIONS]'
           opts.version = VERSION
-          opts.on('-d', '--directory=DIRECTORY', 'Target directory') { |d| @directory = d }
+          opts.on('-a', '--artifact-directory=DIRECTORY', 'Artifact directory') { |a| @artifact_dir = a }
           opts.on('-r', '--runtime=RUNTIME', 'Runtime') { |r| @runtime = r }
+          opts.on('-s', '--source-directory=DIRECTORY', 'Source directory') { |s| @source_dir = d }
           opts.on('-v', '--verbose', 'Enable verbose mode') { @verbose = true }
         end
       end
